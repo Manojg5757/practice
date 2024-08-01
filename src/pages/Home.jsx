b3e737509095a489f4a49ef0b9de170480c887ab
manojvsp <gmanojece2021@gmail.com>
2024-08-01 10:53:09 +0530

    go to about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
         <h1>Welcome Home</h1>
+        <Link to={'/about'}>
+        <button>Go to About</button></Link>
     </div>
   )
 }
